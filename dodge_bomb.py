220152d5bc2a01008c3bbfec5d61139dbc2fff07
nakanishisan <c0a22109e9@edu.teu.ac.jp>
2023-11-28 14:14:35 +0900

    練習２：爆弾を動かす

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,7 +16,8 @@ def main():
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #練習１：赤い半径10の円を作る
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0, WIDTH)
-    bb_rct.centery = random.randint(0, HEIGHT)    
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5 #練習２：爆弾の速度    
     bb_img.set_colorkey((0, 0, 0)) #練習１：黒い部分を透明にする
     clock = pg.time.Clock()
     tmr = 0
@@ -27,10 +28,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
